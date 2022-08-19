1268565bc3897451765551a6fc2560e3e2e4c284
raashy <104453243+raashy@users.noreply.github.com>
2022-08-19 23:38:56 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const io = require("socket.io")(3000, {
+const io = require("socket.io")(8080, {
   cors: {
     origin: ["http://localhost:5173"],
   },
@@ -7,5 +7,9 @@ const io = require("socket.io")(3000, {
 io.on("connection", (socket) => {
   console.log("New client connected");
   socket.on("disconnect", () => console.log("Client disconnected"));
+  socket.on("Text-change", (data) => {
+   
+    socket.broadcast.emit("change", data);
+  })
 });
 
